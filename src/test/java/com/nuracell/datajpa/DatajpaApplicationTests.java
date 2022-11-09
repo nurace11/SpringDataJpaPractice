2c60e416a9d551ad7657683267b68a157c8d740f
NN <qwertylollel@gmail.com>
2022-11-09 21:34:11 +0600

    first commit

---
+++ src/test/java/com/nuracell/datajpa/DatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuracell.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
