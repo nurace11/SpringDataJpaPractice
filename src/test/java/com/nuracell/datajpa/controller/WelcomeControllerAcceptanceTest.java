55972ddd4ff3af19cb8e11c4f32cf3726ac9fb35
NN <qwertylollel@gmail.com>
2022-11-19 19:55:51 +0600

    Acceptance test

---
+++ src/test/java/com/nuracell/datajpa/controller/WelcomeControllerAcceptanceTest.java
@@ -0,0 +1,40 @@
+package com.nuracell.datajpa.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WelcomeControllerAcceptanceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldGetDefaultWelcomeMessage() throws Exception {
+        mockMvc.perform(get("/welcome"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Welcome Unknown!")));
+    }
+
+    @Test
+    void shouldGetCustomWelcomeMessage() throws Exception {
+        mockMvc.perform(get("/welcome?message=Hewlett"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Welcome Hewlett!")));
+    }
+}
